14c48b75922b7996a4786bf783b2c17d6cd9eb5a
Aad Versteden <madnificent@gmail.com>
2019-05-10 11:15:37 +0200

    Allow not sending delta's originating from own service
    A service can now specify the `options.ignoreFromSelf` in order not to
    receive delta messages from queries it executed itself.

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ import { app } from 'mu';
 import request from 'request';
 import services from '/config/rules.js';
 import bodyParser from 'body-parser';
+import dns from 'dns';
 
 // Also parse application/json as json
 app.use( bodyParser.json( { type: function(req) { return /^application\/json/.test( req.get('content-type') ); } } ) );
@@ -43,15 +44,19 @@ app.post( '/', function( req, res ) {
   res.status(204).send();
 } );
 
-function informWatchers( changeSets, changedTriples, res ){
+async function informWatchers( changeSets, changedTriples, res ){
   for( let entry of services ) {
     // for each entity
     if( process.env["DEBUG_DELTA_MATCH"] )
-      console.log(`Checking if we want to send to ${entry.callback.uri}`);
+      console.log(`Checking if we want to send to ${entry.callback.url}`);
 
     let matchSpec = entry.match;
 
-    let someTripleMatchedSpec = changedTriples.some( (triple) => tripleMatchesSpec( triple, matchSpec ) );
+    let originFilteredTriples = await filterMatchesForOrigin( changedTriples, entry );
+
+    let someTripleMatchedSpec =
+        originFilteredTriples
+        .some( (triple) => tripleMatchesSpec( triple, matchSpec ) );
 
     if( process.env["DEBUG_TRIPLE_MATCHES_SPEC"] )
       console.log(`Triple matches spec? ${someTripleMatchedSpec}`);
@@ -59,14 +64,14 @@ function informWatchers( changeSets, changedTriples, res ){
     if( someTripleMatchedSpec ) {
       // inform matching entities
       if( process.env["DEBUG_DELTA_SEND"] )
-        console.log(`Going to send ${entry.callback.method} to ${entry.callback.uri}`);
-
-      let requestSendingFunction = () => sendRequest( entry, changeSets );
+        console.log(`Going to send ${entry.callback.method} to ${entry.callback.url}`);
 
       if( entry.options && request.options.gracePeriod ) {
-        setTimeout( requestSendingFunction, entry.options.gracePeriod );
+        setTimeout(
+          () => sendRequest( entry, changeSets ),
+          entry.options.gracePeriod );
       } else {
-        requestSendingFunction();
+        sendRequest( entry, changeSets );
       }
     }
   }
@@ -86,7 +91,7 @@ function tripleMatchesSpec( triple, matchSpec ) {
       return false;
 
     for( let subKey in subMatchSpec )
-      // we're now matching something like {type: "uri", value: "http..."}
+      // we're now matching something like {type: "url", value: "http..."}
       if( subMatchSpec[subKey] !== subMatchValue[subKey] )
         return false;
   }
@@ -114,23 +119,44 @@ function sendRequest( entry, changeSets ) {
 
   // construct the requestObject
   let method = entry.callback.method;
-  let uri = entry.callback.uri;
+  let url = entry.callback.url;
   if( entry.options && entry.options.resourceFormat ) {
     // we should send contents
     const body = formatChangesetBody( changeSets, entry.options );
 
     requestObject = {
-      uri, method,
+      url, method,
       headers: { "Content-Type": "application/json" },
       body: body
     };
   } else {
     // we should only inform
-    requestObject = { uri, method };
+    requestObject = { url, method };
   }
 
   if( process.env["DEBUG_DELTA_SEND"] )
-    console.log(`Executing send ${method} to ${uri}`);
+    console.log(`Executing send ${method} to ${url}`);
 
   request( requestObject ); // execute request
 }
+
+async function filterMatchesForOrigin( changedTriples, entry ) {
+  if( ! entry.options || !entry.options.ignoreFromSelf ) {
+    return changedTriples;
+  } else {
+    let originIpAddress = await getServiceIp( entry.callback.url );
+    return changedTriples.filter( (change) => change.origin != originIpAddress );
+  }
+}
+
+async function getServiceIp(serviceEndpoint) {
+  const hostName = (new URL(serviceEndpoint)).hostName;
+  return new Promise( (resolve, reject) => {
+    dns.lookup( hostName, { family: 4 }, ( err, address) => {
+      if( err )
+        reject( err );
+      else
+        resolve( address );
+    } );
+  } );
+};
